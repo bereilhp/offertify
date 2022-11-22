d928be34c7a023f75d06466c1fae7de69a5b3227
David Gilarranz Ortega <d.gilarranz1@usp.ceu.es>
2022-11-22 17:33:15 +0100

    express-session descargado

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');

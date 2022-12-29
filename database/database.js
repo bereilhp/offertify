69ad36c3e59a75f622e3425b63bde2598dc84d36
David Gilarranz Ortega <d.gilarranz1@usp.ceu.es>
2022-12-29 11:11:01 +0100

    database -> Test asegura conexión a base de datos correcta

--- database/database.js
+++ database/database.js
@@ -1,4 +1,7 @@
 const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database('');
+const path = require('path');
+const db = new sqlite3.Database(path.join(__dirname, 'database.db'), () => {
+    console.log("Conectado a BBDD");
+});
 
 module.exports = { }
--- tests/database/database.test.js
+++ tests/database/database.test.js
@@ -8,3 +8,18 @@ test('Módulo database se conecta a base de datos', () => {
     expect(db).toBeInstanceOf(sqlite3.Database);
 });
 
+test('Módulo database se conecta a la base de datos adecuada', done => {
+    const db = database.__get__('db');
+    const expectedTables = ['Usuarios', 'Locales', 'Ofertas', 'Reservas', 'Chats', 'Mensajes', 'Resennas'];
+    db.all('SELECT name FROM sqlite_master WHERE type="table"', function (err, tables) {
+        let foundAll = true;
+        for (let i = 0; i < tables.length; i++) {
+            foundAll &&= expectedTables.includes(tables[i].name);
+        }
+        expect(foundAll).toBeTruthy();
+
+        // Indicamos a jest que ya puede evaluar el resultado del callback
+        done();
+        return;
+    });
+});

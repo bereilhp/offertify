daff5f59e5a22b62fa121d0dc8fa72e37f8fec8d
GOnzaloAlvarez <g.alvarez33@usp.ceu.es>
2022-12-30 12:48:16 +0100

    mensajes

---
+++ model/mensajes.js
@@ -0,0 +1,29 @@
+const uuid = require('uuid');
+
+const Mensajes = class Mensajes {
+    constructor(uuid, nombreU, texto, timestamp) {
+       this.uuid = uuid;
+       this.nombreU = nombreU;
+       this.texto = texto;
+       this.timestamp = timestamp; 
+    }
+}
+
+/**
+ * Función para crear locales.
+ *  
+ * @param {string} nombreU Nombre del Usuario
+ * @param {string} texto Contenido del mensaje
+ * @param {int} timestamp tiempo
+ * @param {string | null} mensajeId Opcional. UUID del mensaje. Si no se especifica, se genera uno nuevo
+ * @returns Un objeto mensaje con los datos proporcionados
+ */
+function mensajeFactory(nombreU, texto,timestamp, mensajeId = null) {
+    mensajeId = mensajelId ?? uuid.v4();
+    return new Mensaje(mensajeId, nombreU, texto, timestamp);
+}
+
+module.exports = {
+    Mensaje,
+    mensajeFactory
+}
---
+++ tests/model/mensajes.test.js
@@ -0,0 +1,59 @@
+const mensajes = require('../../model/mensajes');
+const Mensaje = mensajes.Mensaje;
+const mensajeFactory = mensajes.mensajeFactory;
+
+test('Mensaje tiene uuid', () => {
+    const uuid = 'id';
+    const nombreU = 'Pepe';
+    const texto = 'Me gustó mucho su local';
+    const timestamp = 100;
+    const mensaje = new Mnesaje(uuid, nombreU,texto,timestamp);
+    expect(mensaje.uuid).toBe(uuid);
+});
+
+test('Mensaje tiene nombre', () => {
+    const uuid = 'id';
+    const nombreU = 'Pepe';
+    const texto = 'Me gustó mucho su local';
+    const timestamp = 100;
+    const mensaje = new Mnesaje(uuid, nombreU,texto,timestamp);
+    expect(mensaje.nombreU).toBe(nombreU);
+});
+
+test('Mensaje tiene texto', () => {
+    const uuid = 'id';
+    const nombreU = 'Pepe';
+    const texto = 'Me gustó mucho su local';
+    const timestamp = 100;
+    const mensaje = new Mnesaje(uuid, nombreU,texto,timestamp);
+    expect(mensaje.texto).toBe(texto);
+});
+
+test('Mensaje tiene timestamp', () => {
+    const uuid = 'id';
+    const nombreU = 'Pepe';
+    const texto = 'Me gustó mucho su local';
+    const timestamp = 100;
+    const mensaje = new Mnesaje(uuid, nombreU,texto,timestamp);
+    expect(mensaje.timestamp).toBe(timestamp);
+});
+
+
+
+
+
+test('mensajeFactory() crea Mnesajes', () => {
+    const mensaje = localFactory('Nombre', 'Me gustó su menú', 100);
+    expect(mensaje).toBeInstanceOf(Mensaje);
+});
+
+test('mensajeFactory() crea uuids diferentes para cada mensaje', () => {
+    const mensaje_1 = mensajeFactory('Nombre', 'Me gustó su menu', 100);
+    const mensaje_2 = mensajeFactory('Nombre', 'Muy accesible el restaurante', 101);
+    expect(mensaje_1.uuid).not.toEqual(mensaje_2.uuid);
+});    
+
+test('mensajeFactory() crea uuid sólo si no se especifica', () => {
+    const mensaje = mensajeFactory('Nombre', 'Me gustó su menú', 100, 'id');
+    expect(mensaje.uuid).toBe('id');
+});
